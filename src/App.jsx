c4de3fbe36e97e66b47d7756d135fe3163b92a21
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-09-01 14:42:16 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+const movies = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+];
+function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
